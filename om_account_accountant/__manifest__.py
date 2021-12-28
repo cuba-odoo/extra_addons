8f5ae90c1a585fecf8817e79451778a74c97fa8d
Jose Andres Hernandez Bustio <jbustio@gmail.com>
2021-12-28 11:11:51 -0500

    Modulos base para la contabilidad

---
+++ om_account_accountant/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Odoo 14 Accounting',
+    'version': '14.0.7.1.0',
+    'category': 'Accounting',
+    'summary': 'Accounting Reports, Asset Management and Account Budget For Odoo14 Community Edition',
+    'live_test_url': 'https://www.youtube.com/watch?v=Kj4hR7_uNs4',
+    'sequence': '1',
+    'website': 'https://www.odoomates.tech',
+    'author': 'Odoo Mates, Odoo SA',
+    'maintainer': 'Odoo Mates',
+    'license': 'LGPL-3',
+    'support': 'odoomates@gmail.com',
+    'depends': [
+        'accounting_pdf_reports',
+        'om_account_asset',
+        'om_account_budget',
+        'om_account_bank_statement_import'
+        ],
+    'demo': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/account_security.xml',
+        'wizard/change_lock_date.xml',
+        'views/fiscal_year.xml',
+        'views/menu.xml',
+        'views/account_settings.xml',
+        'views/account_type.xml',
+        'views/account_bank_statement.xml',
+        'views/account_coa_template.xml',
+        'views/account_group.xml',
+        'views/account_tag.xml',
+        'views/fiscal_position_template.xml',
+        'views/res_partner.xml',
+        'views/reconciliation.xml',
+        'views/payment_method.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['static/description/banner_new.gif'],
+    'qweb': [],
+}
---
+++ om_account_asset/doc/RELEASE_NOTES.md
@@ -0,0 +1,16 @@
+## Module <om_account_asset>
+
+#### 24.12.2021
+#### Version 14.0.6.0.0
+##### FIX
+- asset
+
+#### 22.12.2021
+#### Version 14.0.5.0.0
+##### IMP
+- remove deprecated method
+
+#### 08.12.2021
+#### Version 14.0.4.0.0
+##### IMP
+- security
